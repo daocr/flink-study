92e990acb4d255c4b6c34aaf595489edc70d07c7
yhl <873104692@qq.com>
2020-11-20 14:52:04 +0800

    添加 MyAggregateFunction

---
+++ data-stream-api/data-stream-api-window/src/main/java/com/huilong/functions/MyAggregateFunction.java
@@ -0,0 +1,140 @@
+package com.huilong.functions;
+
+import com.huilong.mock.MockOrderEvent;
+import com.huilong.mock.source.MockEventSourceFunction;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+/**
+ * 集合计算
+ *
+ * @author daocr
+ * @date 2020/11/20
+ */
+@Slf4j
+public class MyAggregateFunction {
+
+
+    public static void main(String[] args) throws Exception {
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime);
+
+
+        MockEventSourceFunction mockEventSourceFunction = new MockEventSourceFunction()
+                .setUserIds(Arrays.asList(11, 22))
+                .setSleepMax(Duration.ofSeconds(3))
+                .setSleepMin(Duration.ofSeconds(1))
+                .setShowGenData(true);
+
+        DataStreamSource<MockOrderEvent> mockEventDataStreamSource = env.addSource(mockEventSourceFunction);
+
+   /*
+            1、用户id和 商品名称 进行分组
+            2、每10s 统计一次
+         */
+        WindowedStream<MockOrderEvent, Integer, TimeWindow> mockOrderEventIntegerTimeWindowWindowedStream = mockEventDataStreamSource.
+                keyBy(MockOrderEvent::getUserId)
+                .timeWindow(Time.seconds(10));
+
+        // 开始集合计算
+        SingleOutputStreamOperator<MockOrderEvent> aggregate = mockOrderEventIntegerTimeWindowWindowedStream.aggregate(new MockOrderEventMockOrderEventMockOrderEventAggregateFunction());
+
+
+        // 1、普通集合计算
+//        SingleOutputStreamOperator<MockOrderEvent> aggregate1 = mockOrderEventIntegerTimeWindowWindowedStream.aggregate(new MockOrderEventMockOrderEventMockOrderEventAggregateFunction());
+//        aggregate1.print();
+
+
+//        // 2、带窗口的集合计算
+//        SingleOutputStreamOperator<Object> aggregate2 = mockOrderEventIntegerTimeWindowWindowedStream.aggregate(new MockOrderEventMockOrderEventMockOrderEventAggregateFunction(), new MockOrderEventIntegerTimeWindowWindowFunction());
+
+
+        //3、对聚合后的数据，进行整理
+        mockOrderEventIntegerTimeWindowWindowedStream.aggregate(new MockOrderEventMockOrderEventMockOrderEventAggregateFunction(), new MockOrderEventIntegerTimeWindowProcessWindowFunction());
+
+
+        env.execute();
+
+    }
+
+    /**
+     * 集合计算
+     */
+    private static class MockOrderEventMockOrderEventMockOrderEventAggregateFunction implements AggregateFunction<MockOrderEvent, MockOrderEvent, MockOrderEvent> {
+        @Override
+        public MockOrderEvent createAccumulator() {
+            return new MockOrderEvent();
+        }
+
+        @Override
+        public MockOrderEvent add(MockOrderEvent mockOrderEvent, MockOrderEvent mockOrderEvent2) {
+
+            MockOrderEvent mockOrderEvent1 = new MockOrderEvent();
+
+            mockOrderEvent1.setAmount(mockOrderEvent.getAmount() + Optional.ofNullable(mockOrderEvent2).map(MockOrderEvent::getAmount).orElse(0));
+            mockOrderEvent1.setUserId(mockOrderEvent.getUserId());
+
+            return mockOrderEvent1;
+        }
+
+        @Override
+        public MockOrderEvent getResult(MockOrderEvent mockOrderEvent) {
+            return mockOrderEvent;
+        }
+
+        @Override
+        public MockOrderEvent merge(MockOrderEvent mockOrderEvent, MockOrderEvent acc1) {
+
+            mockOrderEvent.setAmount(mockOrderEvent.getAmount() + acc1.getAmount());
+            mockOrderEvent.setUserId(mockOrderEvent.getUserId());
+
+            return mockOrderEvent;
+        }
+    }
+
+    /**
+     * 带窗口对聚合后的数据进行处理
+     */
+    private static class MockOrderEventIntegerTimeWindowWindowFunction implements WindowFunction<MockOrderEvent, Object, Integer, TimeWindow> {
+        @Override
+        public void apply(Integer integer, TimeWindow window, Iterable<MockOrderEvent> input, Collector<Object> out) throws Exception {
+
+            List<MockOrderEvent> collect = StreamSupport.stream(input.spliterator(), false).collect(Collectors.toList());
+
+            log.info("size  : {}  content: {}", collect.size(), collect);
+
+        }
+    }
+
+    /**
+     * 不带窗口对聚合后的数据进行处理
+     */
+    private static class MockOrderEventIntegerTimeWindowProcessWindowFunction extends ProcessWindowFunction<MockOrderEvent, Object, Integer, TimeWindow> {
+        @Override
+        public void process(Integer integer, Context context, Iterable<MockOrderEvent> elements, Collector<Object> out) throws Exception {
+
+            List<MockOrderEvent> collect = StreamSupport.stream(elements.spliterator(), false).collect(Collectors.toList());
+
+            log.info("size  : {}  content: {}", collect.size(), collect);
+        }
+    }
+}
